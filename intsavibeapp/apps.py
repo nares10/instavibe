1d72c6acd4eb38d92ba8487ebb56ba12f06f07cc
Naresh Dewasi <nareshdewasi51@hotmail.com>
2025-07-18 16:42:16 +0530

    connected intsavibeapp to project

---
+++ intsavibeapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IntsavibeappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'intsavibeapp'
---
+++ intsavibeapp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request):
+    return 'Hello, this is the home view of the Instavibe app!'
